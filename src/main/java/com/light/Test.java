72207e33e25b1b6e4aa813c42759ee26621bec05
light <1312992462@qq.com>
2022-07-17 09:25:17 +0800

    first commit

---
+++ src/main/java/com/light/Test.java
@@ -0,0 +1,13 @@
+package com.light;
+
+/**
+ * @Description:
+ * @Author:light
+ * @Date2022/7/17 9:19
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("light");
+    }
+}
