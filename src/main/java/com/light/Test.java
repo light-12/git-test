1de856ebf5952f1b0456b9a9a872acf977da3541
light <1312992462@qq.com>
2022-07-17 09:49:02 +0800

    hot fix1

--- src/main/java/com/light/Test.java
+++ src/main/java/com/light/Test.java
@@ -10,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("light");
         System.out.println("light2");
+        System.out.println("light3");
     }
 }
